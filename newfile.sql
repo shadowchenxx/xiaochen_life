5c1df684a69f7f529efc45475ee97e1cdc1855a2
ShadowChen <31363565+shadowchenxx@users.noreply.github.com>
2019-11-11 17:29:13 +0800

    Create newfile.sql
    一次新的文件添加

---
+++ newfile.sql
@@ -0,0 +1,11 @@
+```sql
+--这是一行测试代码
+select split(regexp_extract(user_agent,'\\;\\\s(.*?)\\;',1),' ')[0] as system_version,
+    count(1) as pv
+from ods.apache
+where ds = '2019-11-10'
+  and hour = '16'
+  and lower(path) like '%/mobileapi/i/device/update%'
+  and user_agent not like 'LR2013Fanli%'
+group by split(regexp_extract(user_agent,'\\;\\\s(.*?)\\;',1),' ')[0];
+```
